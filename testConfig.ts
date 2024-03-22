ed9baf879a5966da3fae388785e85f2ac4f289f9
AmritaSasikumar-CH <asasikumar@companieshouse.gov.uk>
2024-03-22 14:14:51 +0000

    removed unused url

--- testConfig.ts
+++ testConfig.ts
@@ -17,7 +17,6 @@ export const testConfig = {
     addressAutoLookUp: "/sole-trader/correspondence-address-lookup",
     addressSelect: "/sole-trader/select-your-correspondence-address",
     addressManualEntry: "/sole-trader/correspondence-address-manual-entry",
-    //addressConfirm: "http://cidev.aws.chdev.org/register-as-companies-house-authorised-agent/sole-trader/confirm-your-correspondence-address",
     whichSector: "/sole-trader/which-sector",
     whichSectorOther: "/sole-trader/which-sector-other",
   },
