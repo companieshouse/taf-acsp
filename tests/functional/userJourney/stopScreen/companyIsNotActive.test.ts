a8ac1152006fe3c9ba9f18dbc2277daa853a417e
AmritaSasikumar-CH <asasikumar@companieshouse.gov.uk>
2024-04-12 12:00:22 +0100

    Fix issue with nationality screen script

--- tests/functional/userJourney/stopScreen/companyIsNotActive.test.ts
+++ tests/functional/userJourney/stopScreen/companyIsNotActive.test.ts
@@ -16,24 +16,28 @@ let companyNumberPageContext;
 let companyNotActivePageContext;
 
 test.beforeEach("Log in to use ACSP Service", async ({ page }) => {
-    const setUp = new globalSetUp(page);
-    typeOfbusinessContext = new typeOfBusinessPage(page);
-    companyNumberPageContext = new companyNumberPage(page);
-    userActionsContext = new userActions(page);
-    assertionsContext = new Assertions(page);
-    companyNotActivePageContext = new CompanyNotActive(page);
-    await setUp.ACSPUserLogin();
+  const setUp = new globalSetUp(page);
+  typeOfbusinessContext = new typeOfBusinessPage(page);
+  companyNumberPageContext = new companyNumberPage(page);
+  userActionsContext = new userActions(page);
+  assertionsContext = new Assertions(page);
+  companyNotActivePageContext = new CompanyNotActive(page);
+  await setUp.ACSPUserLogin();
 });
 
-test("Verify only active companies can register as ACSPs, @Smoke @StopScreen", async () => {
-    await typeOfbusinessContext.selectTypeOfBusiness(testConfig.limitedCompany);
-    await userActionsContext.clickContinue();
-    await assertionsContext.checkPageTitle(pageTitle.limitedCompanyNumber);
-    await companyNumberPageContext.enterCompanyNumber(userInput.inactiveCompanyNumber);
-    await userActionsContext.clickContinue();
-    await assertionsContext.checkPageTitle(pageTitle.limitedIsThisYourCompany);
-    await userActionsContext.clickConfirmAndContinue();
-    await assertionsContext.checkPageTitle(pageTitle.limitedCompanyInactive);
-    // await assertionsContext.checkElementNotVisible(companyNotActivePageContext.page.getByRole("link", {name: "Back", exact: true}));
-    await assertionsContext.checkElementNotVisible(companyNotActivePageContext.page.getByRole("button", {name: "continue"}));
-})
+test("Verify only active companies can register as ACSPs, @smoke @limited @StopScreen", async () => {
+  await typeOfbusinessContext.selectTypeOfBusiness(testConfig.limitedCompany);
+  await userActionsContext.clickContinue();
+  await assertionsContext.checkPageTitle(pageTitle.limitedCompanyNumber);
+  await companyNumberPageContext.enterCompanyNumber(
+    userInput.inactiveCompanyNumber
+  );
+  await userActionsContext.clickContinue();
+  await assertionsContext.checkPageTitle(pageTitle.limitedIsThisYourCompany);
+  await userActionsContext.clickConfirmAndContinue();
+  await assertionsContext.checkPageTitle(pageTitle.limitedCompanyInactive);
+  // await assertionsContext.checkElementNotVisible(companyNotActivePageContext.page.getByRole("link", {name: "Back", exact: true}));
+  await assertionsContext.checkElementNotVisible(
+    companyNotActivePageContext.page.getByRole("button", { name: "continue" })
+  );
+});
--- tests/functional/userJourney/stopScreen/notAMLRegisteredScreen.test.ts
+++ tests/functional/userJourney/stopScreen/notAMLRegisteredScreen.test.ts
@@ -22,33 +22,41 @@ let amlNameRegisteredPageContext;
 let businessMustBeAMLRegisteredPageContext;
 
 test.beforeEach("Log in to use ACSP Service", async ({ page }) => {
-    const setUp = new globalSetUp(page);
-    typeOfbusinessContext = new typeOfBusinessPage(page);
-    companyNumberPageContext = new companyNumberPage(page);
-    userActionsContext = new userActions(page);
-    whatIsYourRoleContext = new whatIsYourRolePage(page);
-    assertionsContext = new Assertions(page);
-    companyAuthNumberPageContext = new CompanyAuthPage(page);
-    amlNameRegisteredPageContext = new NameRegisteredWithAMLPage(page);
-    businessMustBeAMLRegisteredPageContext = new BusinessMustBeAMLRegistered(page);
-    await setUp.ACSPUserLogin();
+  const setUp = new globalSetUp(page);
+  typeOfbusinessContext = new typeOfBusinessPage(page);
+  companyNumberPageContext = new companyNumberPage(page);
+  userActionsContext = new userActions(page);
+  whatIsYourRoleContext = new whatIsYourRolePage(page);
+  assertionsContext = new Assertions(page);
+  companyAuthNumberPageContext = new CompanyAuthPage(page);
+  amlNameRegisteredPageContext = new NameRegisteredWithAMLPage(page);
+  businessMustBeAMLRegisteredPageContext = new BusinessMustBeAMLRegistered(
+    page
+  );
+  await setUp.ACSPUserLogin();
 });
 
-test("Verify only business' AML registered with the business name can register as ACSPs, @Smoke @StopScreen", async () => {
-    await typeOfbusinessContext.selectTypeOfBusiness(testConfig.limitedCompany);
-    await userActionsContext.clickContinue();
-    await assertionsContext.checkPageTitle(pageTitle.limitedCompanyNumber);
-    await companyNumberPageContext.enterCompanyNumber(userInput.companyNumber);
-    await userActionsContext.clickContinue();
-    await assertionsContext.checkPageTitle(pageTitle.limitedIsThisYourCompany);
-    await userActionsContext.clickConfirmAndContinue();
-    await companyAuthNumberPageContext.enterCompanyAuthNumber(userInput.companyAuthCode);
-    await userActionsContext.clickAuthenticate();
-    await whatIsYourRoleContext.selectRole(testConfig.director);
-    await userActionsContext.clickContinue();
-    await amlNameRegisteredPageContext.selectAMLName(testConfig.yourNameRadio)
-    await userActionsContext.clickContinue();
-    await assertionsContext.checkPageTitle(pageTitle.businessMustBeAMLRegistered);
-    // await assertionsContext.checkElementNotVisible(businessMustBeAMLRegisteredPageContext.page.getByRole("link", {name: "Back", exact: true}));
-    await assertionsContext.checkElementNotVisible(businessMustBeAMLRegisteredPageContext.page.getByRole("button", {name: "continue"}));
-})
+test("Verify only business' AML registered with the business name can register as ACSPs, @smoke @limited @StopScreen", async () => {
+  await typeOfbusinessContext.selectTypeOfBusiness(testConfig.limitedCompany);
+  await userActionsContext.clickContinue();
+  await assertionsContext.checkPageTitle(pageTitle.limitedCompanyNumber);
+  await companyNumberPageContext.enterCompanyNumber(userInput.companyNumber);
+  await userActionsContext.clickContinue();
+  await assertionsContext.checkPageTitle(pageTitle.limitedIsThisYourCompany);
+  await userActionsContext.clickConfirmAndContinue();
+  await companyAuthNumberPageContext.enterCompanyAuthNumber(
+    userInput.companyAuthCode
+  );
+  await userActionsContext.clickAuthenticate();
+  await whatIsYourRoleContext.selectRole(testConfig.director);
+  await userActionsContext.clickContinue();
+  await amlNameRegisteredPageContext.selectAMLName(testConfig.yourNameRadio);
+  await userActionsContext.clickContinue();
+  await assertionsContext.checkPageTitle(pageTitle.businessMustBeAMLRegistered);
+  // await assertionsContext.checkElementNotVisible(businessMustBeAMLRegisteredPageContext.page.getByRole("link", {name: "Back", exact: true}));
+  await assertionsContext.checkElementNotVisible(
+    businessMustBeAMLRegisteredPageContext.page.getByRole("button", {
+      name: "continue",
+    })
+  );
+});
